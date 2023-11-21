1b55655456175bbeae5b5eb94b707baf2a3696d0
Jake Siney <jakesiney@gmail.com>
2023-11-21 12:33:50 +0000

    Fixed ingress

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,20 @@
 resource "aws_instance" "web1" {
-  ami           = "ami-06ce6bb40e50efe77"
-  instance_type = "t2.micro"
+  ami             = "ami-06ce6bb40e50efe77"
+  instance_type   = "t2.micro"
   security_groups = [aws_security_group.TF_SG.name]
-  key_name      = "uploader-key"
+  key_name        = "uploader-key"
   tags = {
     Name = "prometheus-server"
   }
-  user_data =filebase64("${path.moodule}/prometheusInstall.sh")
+  user_data = filebase64("${path.moodule}/prometheusInstall.sh")
 
 }
 
 resource "aws_instance" "web2" {
-  ami           = "ami-06ce6bb40e50efe77"
-  instance_type = "t2.micro"
+  ami             = "ami-06ce6bb40e50efe77"
+  instance_type   = "t2.micro"
   security_groups = [aws_security_group.TF_SG.name]
-  key_name      = "uploader-key"
+  key_name        = "uploader-key"
   tags = {
     Name = "grafana-server"
   }
